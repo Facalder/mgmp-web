2582e1740054beee2928ce327ae443f88a9b44f4
Facalder <faainamacaldera16@gmail.com>
2026-06-29 17:09:48 +0700

    feat: apply shadcn preset and update global styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,9 @@
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono, JetBrains_Mono } from 'next/font/google'
+import { Roboto } from 'next/font/google'
 import './globals.css'
 import { cn } from '@/lib/utils'
 
-const jetbrainsMono = JetBrains_Mono({
-    subsets: ['latin'],
-    variable: '--font-mono'
-})
-
-const geistSans = Geist({
-    variable: '--font-geist-sans',
-    subsets: ['latin']
-})
-
-const geistMono = Geist_Mono({
-    variable: '--font-geist-mono',
-    subsets: ['latin']
-})
+const roboto = Roboto({ subsets: ['latin'], variable: '--font-sans' })
 
 export const metadata: Metadata = {
     title: 'Create Next App',
@@ -34,10 +21,9 @@ export default function RootLayout({
             className={cn(
                 'h-full',
                 'antialiased',
-                geistSans.variable,
-                geistMono.variable,
                 'font-geist-sans',
-                jetbrainsMono.variable
+                'font-sans',
+                roboto.variable
             )}
         >
             <body className='min-h-full flex flex-col'>{children}</body>
